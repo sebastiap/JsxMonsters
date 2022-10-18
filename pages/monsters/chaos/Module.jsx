6b2c8c2274d3d2fdc8bb0ce1799f8d11ad6906a8
sebastiap <sebastiap@gmail.com>
2022-10-17 22:58:10 -0300

    eyes count on Chaos Monster

--- pages/monsters/chaos/Module.jsx
+++ pages/monsters/chaos/Module.jsx
@@ -8,15 +8,29 @@ import { faEye } from '@fortawesome/free-solid-svg-icons'
 
 // Needs content and Onclick properties to work properly.
 
+
+
+
+
 function Module(props) {
 const [animated, setAnimated] = useState(false);
+
+
+var contenido = [];
+if (props.content != undefined) { for (var i = 0; i < props.content; i++) {
+  contenido.push(<FontAwesomeIcon style={{fontSize:"15px"}} icon={faEye} />)
+        }
+        }
+
+
   return (
     <div className={animated ? `${styles.module} ${styles.clicked}` : `${styles.chaosmodule}`}
     onClick={() => setAnimated(() => true, props.onClick())}
     onAnimationEnd={() => setAnimated(() => false)}
     >
       {/* {props.content} */}
-      <FontAwesomeIcon icon={faEye} />
+      {contenido.map(ojo => ojo)}
+      {/* <FontAwesomeIcon icon={faEye} /> */}
 
       </div>
   )
